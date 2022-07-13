15107624df1e3d3b7e7a64c1ff07633a439080a2
Kat Brandt <joyfulkat11@gmail.com>
2022-07-13 17:21:16 -0600

    Adds class method for with_long_songs.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -4,4 +4,10 @@ class Artist < ApplicationRecord
   def average_song_length
     self.songs.average(:length)
   end
+
+  def self.with_long_songs
+    joins(:songs)
+    .where('songs.length > 400')
+    .distinct
+  end
 end

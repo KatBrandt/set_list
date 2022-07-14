069703a8e46e319161ab06a3a52b3bb3224763da
Kat Brandt <joyfulkat11@gmail.com>
2022-07-14 16:08:14 -0600

    Finishes up validation testing.

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,4 +2,5 @@ class Song < ApplicationRecord
   belongs_to :artist
 
   validates :title, length: { maximum: 500 }
+  validates_numericality_of :play_count, only_integer: true
 end
--- spec/models/song_spec.rb
+++ spec/models/song_spec.rb
@@ -7,5 +7,7 @@ RSpec.describe Song, type: :model do
 
   describe 'validations' do
     it { should validate_length_of(:title).is_at_most(500) }
+    it { should validate_numericality_of(:play_count).only_integer }
+    it { should validate_numericality_of(:play_count).on(:update) }
   end
 end

8a73b9255872e2b78d4d16d4a6f78d9f63c416b5
Kat Brandt <joyfulkat11@gmail.com>
2022-07-14 16:31:19 -0600

    Sets up relationship for playlists and songs.

--- spec/models/playlist_spec.rb
+++ spec/models/playlist_spec.rb
@@ -3,5 +3,6 @@ require 'rails_helper'
 describe Playlist, type: :model do
   describe 'relationships' do
     it { should have_many :playlist_songs }
+    it { should have_many(:songs).through :playlist_songs }
   end
 end

5f5582feff9f5eeb40d6e2b386631696b4bc97a1
Kat Brandt <joyfulkat11@gmail.com>
2022-07-16 14:25:57 -0600

    Adds method to find artists longest song length.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -2,10 +2,6 @@ class Artist < ApplicationRecord
   has_many :songs
   validates_uniqueness_of :name
 
-  def average_song_length
-    songs.average(:length).to_f
-  end
-
   def self.with_long_songs
     joins(:songs)
     .where('songs.length > 400')
@@ -17,6 +13,7 @@ class Artist < ApplicationRecord
     .select('artists.*, max(length) as longest')
     .group(:id)
     # how to have this in the right format?
+    #rework into an instance method
   end
 
   def self.longest_song
@@ -24,5 +21,16 @@ class Artist < ApplicationRecord
     .order('longest desc')
     .first
     .name
+  end
+
+  def average_song_length
+    songs.average(:length).to_f
+  end
+
+  def longest_song_length
+    Artist.longest_song_lengths
+    .where(id: self.id)
+    .first
+    .longest
   end
 end
--- spec/models/artist_spec.rb
+++ spec/models/artist_spec.rb
@@ -40,6 +40,31 @@ RSpec.describe Artist do
         expect(@prince.average_song_length.round(2)).to eq(503.67)
       end
     end
+
+    describe '#longest_song_length' do
+      it 'returns the length of their longest song' do
+        @prince = Artist.create!(name: 'Prince')
+        @rtj = Artist.create!(name: 'Run The Jewels')
+        @caamp = Artist.create!(name: 'Caamp')
+        @billie = Artist.create!(name: 'Billie Eilish')
+
+        @prince.songs.create!(title: 'Purple Rain', length: 845, play_count: 8599)
+        @prince.songs.create!(title: 'Raspberry Beret', length: 665, play_count: 99)
+
+        @rtj.songs.create!(title: 'Legend Has It', length: 2301, play_count: 2300000)
+        @rtj.songs.create!(title: 'Talk to Me', length: 2305, play_count: 2300000)
+
+        @caamp.songs.create!(title: '26', length: 940, play_count: 150000)
+        @caamp.songs.create!(title: 'Vagabond', length: 240, play_count: 120000)
+
+        @billie.songs.create!(title: 'bury a friend', length: 340, play_count: 1200000)
+        @billie.songs.create!(title: 'bad guy', length: 240, play_count: 100000)
+
+        expect(@billie.longest_song_length).to eq 340
+        expect(@prince.longest_song_length).to eq 845
+        expect(@rtj.longest_song_length).to eq 2305
+      end
+    end
   end
 
   describe 'class methods' do

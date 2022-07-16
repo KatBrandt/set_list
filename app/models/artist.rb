63949d11e7545c412e2955616e639fdf6302ab0a
Kat Brandt <joyfulkat11@gmail.com>
2022-07-15 23:34:50 -0600

    Adds longest_song class method.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -3,12 +3,26 @@ class Artist < ApplicationRecord
   validates_uniqueness_of :name
 
   def average_song_length
-    self.songs.average(:length)
+    songs.average(:length).to_f
   end
 
   def self.with_long_songs
     joins(:songs)
     .where('songs.length > 400')
     .distinct
+  end
+
+  def self.longest_song_lengths
+    test = joins(:songs)
+    .select('artists.*, max(length) as longest')
+    .group(:id)
+    # how to have this in the right format?
+  end
+
+  def self.longest_song
+    longest_song_lengths
+    .order('longest desc')
+    .first
+    .name
   end
 end
--- spec/models/artist_spec.rb
+++ spec/models/artist_spec.rb
@@ -9,24 +9,10 @@ RSpec.describe Artist do
     it { should validate_uniqueness_of :name}
   end
 
+
   describe 'instance methods' do
     describe '#average_song_length' do
-      before :each do
-        @prince = Artist.create!(name: 'Prince')
-        @purple = @prince.songs.create!(title: 'Purple Rain', length: 845, play_count: 8599)
-        @beret = @prince.songs.create!(title: 'Raspberry Beret', length: 664, play_count: 99)
-        @other_song = @prince.songs.create!(title: 'Another Prince Song', length: 1, play_count: 99)
-      end
-
       it 'returns the average song length' do
-        expect(@prince.average_song_length.round(2)).to eq(503.33)
-      end
-    end
-  end
-
-  describe 'class methods' do
-    describe '.with_long_songs' do
-      it 'returns artists who have songs longer than 400' do
         @prince = Artist.create!(name: 'Prince')
         @rtj = Artist.create!(name: 'Run The Jewels')
         @caamp = Artist.create!(name: 'Caamp')
@@ -49,9 +35,96 @@ RSpec.describe Artist do
         @lcd.songs.create!(title: 'Someone Great', length: 500, play_count: 1000000)
         @lcd.songs.create!(title: 'I Can Change', length: 640, play_count: 100000)
 
+        @other_song = @prince.songs.create!(title: 'Another Prince Song', length: 1, play_count: 99)
+
+        expect(@prince.average_song_length.round(2)).to eq(503.67)
+      end
+    end
+  end
+
+  describe 'class methods' do
+    describe '.with_long_songs' do
+      it 'returns artists who have songs longer than 400' do
+
+        @prince = Artist.create!(name: 'Prince')
+        @rtj = Artist.create!(name: 'Run The Jewels')
+        @caamp = Artist.create!(name: 'Caamp')
+        @jgb = Artist.create!(name: 'Jerry Garcia Band')
+        @billie = Artist.create!(name: 'Billie Eilish')
+        @lcd = Artist.create!(name: 'LCD Soundsystem')
+
+        @prince.songs.create!(title: 'Purple Rain', length: 845, play_count: 8599)
+        @prince.songs.create!(title: 'Raspberry Beret', length: 665, play_count: 99)
+
+        @rtj.songs.create!(title: 'Legend Has It', length: 2301, play_count: 2300000)
+        @rtj.songs.create!(title: 'Talk to Me', length: 2301, play_count: 2300000)
+
+        @caamp.songs.create!(title: '26', length: 940, play_count: 150000)
+        @caamp.songs.create!(title: 'Vagabond', length: 240, play_count: 120000)
+
+        @billie.songs.create!(title: 'bury a friend', length: 340, play_count: 1200000)
+        @billie.songs.create!(title: 'bad guy', length: 240, play_count: 100000)
+
+        @lcd.songs.create!(title: 'Someone Great', length: 500, play_count: 1000000)
+        @lcd.songs.create!(title: 'I Can Change', length: 640, play_count: 100000)
+
+
         expect(Artist.with_long_songs).to match_array [@prince, @rtj, @caamp, @lcd]
         expect(Artist.with_long_songs).to contain_exactly @prince, @rtj, @caamp, @lcd
         # with an array when we don't care about the order of the returned array, can use match_array or contain_exactly instead of eq
+      end
+    end
+
+    describe '.longest_song_lengths' do
+      it 'returns the length of each artist\'s longest song' do
+        @prince = Artist.create!(name: 'Prince')
+        @rtj = Artist.create!(name: 'Run The Jewels')
+        @caamp = Artist.create!(name: 'Caamp')
+        @billie = Artist.create!(name: 'Billie Eilish')
+
+        @prince.songs.create!(title: 'Purple Rain', length: 845, play_count: 8599)
+        @prince.songs.create!(title: 'Raspberry Beret', length: 665, play_count: 99)
+
+        @rtj.songs.create!(title: 'Legend Has It', length: 2305, play_count: 2300000)
+        @rtj.songs.create!(title: 'Talk to Me', length: 2301, play_count: 2300000)
+
+        @caamp.songs.create!(title: '26', length: 940, play_count: 150000)
+        @caamp.songs.create!(title: 'Vagabond', length: 240, play_count: 120000)
+
+        @billie.songs.create!(title: 'bury a friend', length: 340, play_count: 1200000)
+        @billie.songs.create!(title: 'bad guy', length: 240, play_count: 100000)
+
+        expected = {
+          @prince => 845,
+          @rtj => 2305,
+          @caamp => 940,
+          @billie => 340
+        }
+
+        # expect(Artist.longest_song_lengths). to eq expected
+      end
+    end
+
+    describe '.longest_song' do
+      it 'returns name of artist with longest song' do
+        @prince = Artist.create!(name: 'Prince')
+        @rtj = Artist.create!(name: 'Run The Jewels')
+        @caamp = Artist.create!(name: 'Caamp')
+        @billie = Artist.create!(name: 'Billie Eilish')
+
+        @prince.songs.create!(title: 'Purple Rain', length: 845, play_count: 8599)
+        @prince.songs.create!(title: 'Raspberry Beret', length: 665, play_count: 99)
+
+        @rtj.songs.create!(title: 'Legend Has It', length: 2305, play_count: 2300000)
+        @rtj.songs.create!(title: 'Talk to Me', length: 2301, play_count: 2300000)
+
+        @caamp.songs.create!(title: '26', length: 940, play_count: 150000)
+        @caamp.songs.create!(title: 'Vagabond', length: 240, play_count: 120000)
+
+        @billie.songs.create!(title: 'bury a friend', length: 340, play_count: 1200000)
+        @billie.songs.create!(title: 'bad guy', length: 240, play_count: 100000)
+
+        expect(Artist.longest_song).to eq "Run The Jewels"
       end
     end
   end

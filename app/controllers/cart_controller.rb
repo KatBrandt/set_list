9d4c155118fdc1d7c30c2dd3036b4a745018ccc2
Kat Brandt <joyfulkat11@gmail.com>
2022-07-18 22:41:56 -0600

    Adds ability to update cart number.

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -1,7 +1,15 @@
 class CartController < ApplicationController
+  include ActionView::Helpers::TextHelper
+
   def update
     song = Song.find(params[:song_id])
-    flash[:notice] = "You now have 1 copy of #{song.title} in your cart."
+
+    song_id_str = song.id.to_s
+    session[:cart] ||= Hash.new(0)
+    session[:cart][song_id_str] ||= 0
+    quantity = session[:cart][song_id_str] += 1
+
+    flash[:notice] = "You now have #{pluralize(quantity, 'copy')} of #{song.title} in your cart."
     redirect_to songs_path
   end
 end
--- spec/features/cart/add_song_spec.rb
+++ spec/features/cart/add_song_spec.rb
@@ -13,4 +13,26 @@ describe 'When a user adds songs to their cart' do
 
     expect(page).to have_content("You now have 1 copy of #{song.title} in your cart.")
   end
+
+  it 'increments for multiple songs and updates message' do
+    artist = Artist.create(name: 'Rick Astley')
+    song1 = artist.songs.create(title: 'Never Gonna Give You Up', length: 250, play_count: 100000)
+    song2 = artist.songs.create(title: "Don't Stop Believin'", length: 300, play_count: 1)
+
+    visit songs_path
+
+    within("#song-#{song1.id}") do
+      click_on "Add Song"
+    end
+
+    within("#song-#{song2.id}") do
+      click_on "Add Song"
+    end
+
+    within("#song-#{song1.id}") do
+      click_on "Add Song"
+    end
+
+    expect(page).to have_content "You now have 2 copies of #{song1.title} in your cart."
+  end
 end
